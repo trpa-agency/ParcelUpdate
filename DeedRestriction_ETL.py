28653d725432fa08af0c3a2bd4c45ab06292278c
trpa-mason <mbindl@trpa.org>
2023-10-18 15:57:14 -0700

    add GradingExceptions_to_ParcelFeatures

--- DeedRestriction_ETL.py
+++ DeedRestriction_ETL.py
@@ -1,2 +1,5 @@
 import pandas as pd
 import sqlalchemy
+
+
+dfDeed     = pd.read_json("https://laketahoeinfo.org/WebServices/GetDeedRestrictedParcels/JSON/e17aeb86-85e3-4260-83fd-a2b32501c476")
---
+++ GradingExceptions_to_ParcelFeatures.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+accelaFiles = ""
+dfGrade = dfGrade    = pd.read_csv(os.path.join(accelaFiles, "Grade.csv"))
