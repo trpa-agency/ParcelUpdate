5f542d18fc4fe7b56d660fb1de58c0126f7b9a27
amcclarytrpa <amcclary@trpa.gov>
2024-10-10 10:44:01 -0700

    Took out parcel point updated because it was giving a bug

--- CountyParcel_Load.py
+++ CountyParcel_Load.py
@@ -115,8 +115,8 @@ utils.update_parcel_layer(parcelNew, master_fc_path,prefix_remove, data_type_map
 utils.update_parcel_layer(parcelNew, base_fc_path,prefix_remove, data_type_mapping, fields_to_exclude_base, fields_to_ignore_base,
                           df_special_parcels,base_difference_csv, database_connection, version_name_full)
 #Update Parcel Points - maybe just generate parcel points? do we need to keep track of edit date for that
-utils.update_parcel_layer(parcelNew_points, points_fc_path,prefix_remove, data_type_mapping, fields_to_exclude_points, fields_to_ignore_points,
-                          df_special_parcels,points_difference_csv, database_connection, version_name_full)
+# utils.update_parcel_layer(parcelNew_points, points_fc_path,prefix_remove, data_type_mapping, fields_to_exclude_points, fields_to_ignore_points,
+#                           df_special_parcels,points_difference_csv, database_connection, version_name_full)
 
 
 #We need to create a connection to Base
